d34c716ebab5847cd9d93ed5e0555f5206f11eed
Kenneth Chung <kennethchung5@gmail.com>
2018-11-11 10:22:57 -0500

    added layout and styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,37 +15,55 @@ var database = firebase.database();
 $("form").on("submit", function(event) {
     event.preventDefault();
 
-    var newTrainName = $("#trainNameInput").val().trim();
-    var newDestination = $("#destinationInput").val().trim();
-    var newFrequency = $("#frequencyInput").val().trim();
-    var newFirstTrain = $("#firstTrainInput").val().trim();
-
     var newEntry = {
-        trainName: newTrainName,
-        destination: newDestination,
-        frequency: newFrequency,
-        firstTrain: newFirstTrain
+        name: $("#nameInput").val().trim(),
+        destination: $("#destinationInput").val().trim(),
+        frequency: $("#frequencyInput").val().trim(),
+        start: $("#startInput").val().trim()
     };
 
     database.ref().push(newEntry);
 
-    $("#trainNameInput").text("");
-    $("#destinationInput").text("");
-    $("#frequencyInput").text("");
-    $("#firstTrainInput").text("");
+    $("#nameInput").val("");
+    $("#destinationInput").val("");
+    $("#frequencyInput").val("");
+    $("#startInput").val("");
 
 });
 
 database.ref().on("child_added", function(snapshot) {
-    console.log(snapshot.val());
 
-    // record firebase values in variables
-    // perform arithmetic operations using moment()
-    // display in html
+    //** REVISIT TIME CALCULATION
 
-    var trainName = snapshot.val().trainName;
-    var destination = snapshot.val().destination;
-    var frequency = snapshot.val().frequency;
-    var firstTrain = snapshot.val().firstTrain;
+    var newName = snapshot.val().name;
+    var newDestination = snapshot.val().destination;
+    var newFrequency = snapshot.val().frequency;
+    var newStart = snapshot.val().start;
+
+    console.log("newFrequency: " + newFrequency);
+    console.log("newStart: " + newStart);
+
+    // var newStartConverted = moment(newStart, "HH:mm").subtract(1, "years");
+    var newStartConverted = moment(newStart, "HH:mm");
+    console.log("newStartConverted: " + newStartConverted);
+
+    var currentTime = moment();
+    console.log("currentTime (formatted): " + moment(currentTime).format("hh:mm"));
+
+    var diffTime = moment().diff(moment(newStartConverted), "minutes");
+    console.log("diffTime: " + diffTime);
+    
+    var tRemainder = diffTime % newFrequency;
+    console.log("tRemainder: " + tRemainder);
+
+    var timeToNext = newFrequency - tRemainder;
+    console.log("timeToNext: " + timeToNext);
+
+    var nextTrain = moment().add(timeToNext, "minutes");
+    console.log("nextTrain (formatted): " + moment(nextTrain).format("hh:mm"));
+    console.log("----")
+   
+
+    
 
 })
