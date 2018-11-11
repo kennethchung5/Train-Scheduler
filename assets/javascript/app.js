5deaac56c367ef5ace2014710b7e61618afad153
Kenneth Chung <kennethchung5@gmail.com>
2018-11-11 15:07:06 -0500

    added row styling to indicate whether train is currently running and apply alternating background color

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,6 +11,8 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
+// variable to track parity of table rows
+var rowCounter = 1;
 
 $("form").on("submit", function(event) {
     event.preventDefault();
@@ -47,25 +49,28 @@ database.ref().on("child_added", function(snapshot) {
     var timeDiff = moment().diff(moment(newStart, "HH:mm"), "minutes");
     // console.log("The time difference is " + timeDiff + " minutes.");
 
+    var newRow = $("<tr>");
+
     // check sign of timeDiff; if negative, then consider train to be not currently running
     if (timeDiff < 0) {
-        timeToNext = -1 * timeDiff;
-        
+        timeToNext = -1 * timeDiff;        
         nextTrain = moment(newStart, "HH:mm").format("hh:mm A");        
+
+        newRow.addClass("outService");
     }
     else {
-        timeToNext = newFrequency - (timeDiff % newFrequency);
-        
+        timeToNext = newFrequency - (timeDiff % newFrequency);        
         nextTrain = moment().add(timeToNext, "minutes").format("hh:mm A");
-    }
+
+        newRow.addClass("inService");
+    };
 
     // console.log("The next train will arrive in " + timeToNext + " minutes.");
     // console.log("The next train will arrive at " + nextTrain + ".");
     // console.log("---")
 
     
-
-    var newRow = $("<tr>").append(
+    newRow.append(
         $("<td class='leftText'>").text(newName),
         $("<td class='leftText'>").text(newDestination),
         $("<td>").text(moment(newStart, "HH:mm").format("hh:mm A")),
@@ -73,6 +78,16 @@ database.ref().on("child_added", function(snapshot) {
         $("<td>").text(nextTrain),
         $("<td>").text(timeToNext)
     );
+
+    // alternate class assignment (for styling) using parity of rowCounter
+    if (rowCounter % 2) {
+        newRow.addClass("oddRow");
+    }
+    else {
+        newRow.addClass("evenRow");
+    };
+
+    rowCounter++;
 
     $("#trainTable > tbody").append(newRow);
 
